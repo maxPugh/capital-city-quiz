71abfbb86a91ff97633019f87cc2aff95b2df754
Max Pugh <46350013+maxPugh@users.noreply.github.com>
2023-09-21 10:27:08 +0100

    added config timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,6 +72,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://127.0.0.1:5173',
+    timeout: 120000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
